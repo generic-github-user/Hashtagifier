318d0543a2ac6b66bbd5cd73874a742bbdeab1c1
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2018-10-27 17:56:06 -0400

    Added smooth increase/decrease of hashtag frequency
    All positions are not re-randomized every time text is added or deleted or hashtag frequency is changed

--- script.js
+++ script.js
@@ -1,22 +1,51 @@
 $("#input").on("input", hashtagify);
 $("#slider").on("input", hashtagify);
 
-var input;
+var tags = [];
+var frequency = 0.1;
+var last_frequency = 0.1;
+var last_input = [];
 
 function hashtagify() {
-      input = $("#input")[0].value;
+      frequency = $("#slider")[0].value / 100;
+      var input = $("#input")[0].value;
+      input = input.replace(/#/g, "");
       if (input != undefined && input != "") {
             input = input.split(" ");
-            var output = "";
-            for (var i = 0; i < input.length; i++) {
-                  if (Math.random() < 0.1) {
-                        output += "#";
-                  }
-                  output += input[i];
-                  if (i < input.length - 1) {
-                        output += " ";
+
+            if (input.length > last_input.length) {
+                  for (var i = 0; i < input.length - last_input.length; i++) {
+                        if (Math.random() < frequency) {
+                              tags.push(1);
+                        } else {
+                              tags.push(0);
+                        }
                   }
             }
-            $("#input")[0].value = output;
+
+            for (var i = 0; i < tags.length; i++) {
+                  if (Math.random() < frequency) {
+                        if (tags[i] == 0 && frequency > last_frequency) {
+                              tags[i] = 1;
+                        } else if (tags[i] == 1 && frequency < last_frequency) {
+                              tags[i] = 0;
+                        }
+                  }
+            }
       }
+
+      var output = "";
+      for (var i = 0; i < input.length; i++) {
+            if (tags[i] == 1) {
+                  output += "#";
+            }
+            output += input[i];
+            if (i < input.length - 1) {
+                  output += " ";
+            }
+      }
+      $("#input")[0].value = output;
+
+      last_input = input;
+      last_frequency = frequency;
 }
