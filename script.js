4b7d1044efd2d2f3290e4e50a6b364f0bf406c5a
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2018-10-27 21:41:53 -0400

    Switched to word-based model for hashtag position processing
    Text added or removed in middle of input will not offset state of other words as it would with a list of hashtag positions. Words are directly checked for hashtags instead of referring to the "tags" array.

--- script.js
+++ script.js
@@ -6,19 +6,33 @@ var frequency = 0.1;
 var last_frequency = 0.1;
 var last_input = [];
 
+function addtag(string) {
+      if (string[0] !== "#") {
+            string = "#" + string;
+      }
+      return string;
+}
+
+function removetag(string) {
+      if (string[0] == "#") {
+            string = string.substr(1);
+      }
+      return string;
+}
+
 function hashtagify() {
       frequency = $("#slider")[0].value / 100;
       var input = $("#input")[0].value;
-      input = input.replace(/#/g, "");
+      // input = input.replace(/#/g, "");
       if (input != undefined && input != "") {
             input = input.split(" ");
 
             if (input.length > last_input.length) {
-                  for (var i = 0; i < input.length - last_input.length; i++) {
+                  for (var i = last_input.length; i < input.length; i++) {
                         if (Math.random() < frequency) {
-                              tags.push(1);
+                              input[i] = addtag(input[i]);
                         } else {
-                              tags.push(0);
+                              input[i] = removetag(input[i]);
                         }
                   }
             }
@@ -28,8 +42,8 @@ function hashtagify() {
             // Number of words without hashtags
             var w_nh = 0;
             // Count number of words with and without hashtags
-            for (var i = 0; i < tags.length; i++) {
-                  if (tags[i] == 1) {
+            for (var i = 0; i < input.length; i++) {
+                  if (input[i][0] == "#") {
                         w_h++;
                   } else {
                         w_nh++;
@@ -42,24 +56,24 @@ function hashtagify() {
             // Calculate probability of removing hashtag
             var p_nh = (((1 - frequency) * w_t) - w_nh) / w_t;
             // Loop through each word in input text
-            for (var i = 0; i < tags.length; i++) {
+            for (var i = 0; i < input.length; i++) {
                   // Check if hashtag frequency setting has increased
                   if (frequency > last_frequency) {
                         // Current word does not have a hashtag
-                        if (tags[i] == 0) {
+                        if (input[i][0] !== "#") {
                               if (Math.random() < p_h) {
                                     // Add a hashtag to the word
-                                    tags[i] = 1;
+                                    input[i] = addtag(input[i]);
                               }
                         }
                   }
                   // Hashtag frequency setting has decreased
                   if (frequency < last_frequency) {
                         // Current word has a hashtag
-                        if (tags[i] == 1) {
+                        if (input[i][0] == "#") {
                               if (Math.random() < p_nh) {
                                     // Remove hashtag from word
-                                    tags[i] = 0;
+                                    input[i] = removetag(input[i]);
                               }
                         }
                   }
@@ -68,9 +82,6 @@ function hashtagify() {
 
       var output = "";
       for (var i = 0; i < input.length; i++) {
-            if (tags[i] == 1) {
-                  output += "#";
-            }
             output += input[i];
             if (i < input.length - 1) {
                   output += " ";
