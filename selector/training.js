693edc179abd38e372248417522984e8018c7c16
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2018-10-28 20:22:21 -0400

    Consistent word vector length; now limited to 15

--- selector/training.js
+++ selector/training.js
@@ -39,7 +39,7 @@ function longest(array) {
 
 const charset = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", " "];
 
-var longest_word = Math.max(longest(hashtag), longest(no_hashtag));
+var longest_word = 15;
 
 for (var i = 0; i < hashtag.length; i++) {
       hashtag[i] = encodeString(hashtag[i], charset, longest_word);
